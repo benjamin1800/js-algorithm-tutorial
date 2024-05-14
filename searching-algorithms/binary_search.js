17afcfb47ccbfb864bffa6fd872253cfe639285c
emmanuelbenjamin <emmanuelbenjamin1800@gmail.com>
2024-05-14 02:24:42 +0100

    recursive search

--- searching-algorithms/binary_search.js
+++ searching-algorithms/binary_search.js
@@ -1,18 +1,18 @@
-//Binary Search Iterative Method 
-function iterativeBinarySearch(arr, x) {
-    let low = 0
-    let high = arr.length - 1
-    while (low <= high){
-        let mid = (low + high)/2
-        if (x == arr[mid]) {
-            return mid
-        } else if (x > arr[mid]) {
-            low = mid + 1
-        } else {
-            high = mid - 1
-        }   
-    }
-}
+//Binary Search Recursive Method
+function recursiveBinarySearch(arr, x, low, high){
+     if (low + high) {
+          return false; 
+     }else {
+          mid = (low + high) / 2
+     }
+      if (x == arr[mid]) {
+          return mid
+     } else if (x > arr[mid]) {
+     return recursiveBinarySearch(arr, x, mid + 1, high)
+ } else {
+     return recursiveBinarySearch(arr, x, low, mid - 1)
+ }
 
-const array = [3, 4, 5, 6, 7, 8, 9]
-console.log(iterativeBinarySearch(array, 8))
+}
+ const array = [3, 4, 5, 6, 7, 8, 9]
+console.log (recursiveBinarySearch(array, 6))
