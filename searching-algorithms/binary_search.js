7928fe7598ef70955dee299423ab78152c65109a
benjamin1800 <165905075+benjamin1800@users.noreply.github.com>
2024-05-15 20:12:06 +0100

    Commit 1.1: Added recursive search (#3)
    * search method
    
    * search method
    
    * binary search
    
    * adjusted

--- searching-algorithms/binary_search.js
+++ searching-algorithms/binary_search.js
@@ -1,17 +1,15 @@
 //Binary Search Recursive Method
-function recursiveBinarySearch(arr, x, low, high){
-     if (low == high) {
-          return true; 
-     }else {
-          mid = (low + high) / 2
-     }
-      if (x == arr[mid]) {
-          return mid
-     } else if (x >= arr[mid]) {
-     return recursiveBinarySearch(arr, x, mid + 1)
- } else {
-     return recursiveBinarySearch(arr, x, mid -1)
- }
+function recursiveBinarySearch(arr, x, low, high) {
+  if (low > high) {
+    return false;
+  } else {
+    let mid = (low + high) / 2;
+    if (x == arr[mid]) {
+      return mid;
+    } else if (x > arr[mid]) {
+      return recursiveBinarySearch(arr, x, mid + 1, high);
+    } else {
+      return recursiveBinarySearch(arr, x, low, mid - 1)
+    }
+  }
 }
- const array = [3, 4, 5, 6, 7, 8, 9]
-console.log (recursiveBinarySearch(array, 6))
